c83b1ab77fcca8b145de4af6a46bc7177eac5347
vishnuvkamath <vishvkamath@gmail.com>
2021-07-18 11:14:39 +0530

    modify docker image name

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
-docker build -t nucoxia/multi-client:latest -t nucoxia/multi-client:$SHA ./client/Dockerfile ./client
-docker build -t nucoxia/multi-server:latest -t nucoxia/multi-server:$SHA ./server/Dockerfile ./server
-docker build -t nucoxia/multi-worker:latest -t nucoxia/multi-worker:$SHA ./worker/Dockerfile ./worker
+docker build -t nucoxia/multi-client-k8s:latest -t nucoxia/multi-client-k8s:$SHA ./client/Dockerfile ./client
+docker build -t nucoxia/multi-server-k8s:latest -t nucoxia/multi-server-k8s:$SHA ./server/Dockerfile ./server
+docker build -t nucoxia/multi-worker-k8s:latest -t nucoxia/multi-worker-k8s:$SHA ./worker/Dockerfile ./worker
 
-docker push nucoxia/multi-client:latest
-docker push nucoxia/multi-server:latest
-docker push nucoxia/multi-worker:latest
+docker push nucoxia/multi-client-k8s:latest
+docker push nucoxia/multi-server-k8s:latest
+docker push nucoxia/multi-worker-k8s:latest
 
-docker push nucoxia/multi-client:$SHA
-docker push nucoxia/multi-server:$SHA
-docker push nucoxia/multi-worker:$SHA
+docker push nucoxia/multi-client-k8s:$SHA
+docker push nucoxia/multi-server-k8s:$SHA
+docker push nucoxia/multi-worker-k8s:$SHA
 
 
 kubectl apply -f k8s
 
-kubectl set image deployment/client-deployment client=nucoxia/multi-client:$SHA
-kubectl set image deployment/server-deployment server=nucoxia/multi-server:$SHA
-kubectl set image deployment/worker-deployment worker=nucoxia/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=nucoxia/multi-client-k8s:$SHA
+kubectl set image deployments/server-deployment server=nucoxia/multi-server-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=nucoxia/multi-worker-k8s:$SHA
 
 
